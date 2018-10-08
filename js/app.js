e5bf781a9449a8769f256f22992d1c74b586435f
AllanJeremy <aj.dev254@gmail.com>
2018-10-08 21:48:48 +0300

    Tut 3: Data and methods in vue instances

--- js/app.js
+++ js/app.js
@@ -3,5 +3,16 @@ new Vue({
     data:{
         title:'Some title',
         description:'Some description about the thing whose name shall not be mentioned',
+    },
+    methods:{
+        sayHi: function(name){
+            let returnable = '';
+            if(name === 'Allan Jeremy')
+            {
+                returnable = 'Welcome back good sir! ';
+            }
+            returnable += "Well hello there "+name+" "+this.title;
+            return returnable;
+        }
     }
 });
