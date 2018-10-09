09370b8d5ec2e0c842f2bed57fc15b68b50c768f
AllanJeremy <aj.dev254@gmail.com>
2018-10-09 13:29:32 +0300

    Tut 5: Events - v-on:evt - longform vue directive for event handling - @evt - shorthand

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 new Vue({
     el:'#app',
     data:{
+        todoItem: '',
         link: 'https://www.allanjeremy.com',
-        someHtml: '<b>Some fancy</b> html<br/>',
         title:'Some title',
         description:'Some description about the thing whose name shall not be mentioned',
+        x:0,
+        y:0
     },
     methods:{
         sayHi: function(name){
-            let returnable = '';
+            var returnable = '';
             if(name === 'Allan Jeremy')
             {
                 returnable = 'Welcome back good sir! ';
             }
             returnable += "Well hello there "+name+" "+this.title;
             return returnable;
+        },
+        reverseText: function()
+        {
+            this.title = this.title.split('').reverse().join('');
+            this.description  = this.description.split('').reverse().join('');
+        },
+        updateCoordinates: function(evt)
+        {
+            this.x = evt.offsetX;
+            this.y = evt.offsetY;
+        },
+        movePlayer: function(evt)
+        {
+            alert(evt);
         }
     }
+    
 });
