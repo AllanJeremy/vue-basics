8eb99b960b71b2f6c1d58669a11672c34013ce1f
AllanJeremy <aj.dev254@gmail.com>
2018-10-10 04:21:15 +0300

    Tut 12: Loops - v-for - v-for getting index with (item,index) where item represents a single item in the array being looped through
    - <template> tag for when we want to  only display the contents of  the tag and not the wrapper tag itself
    
    - v-for on an object with (val,key) for getting value and keys of the object

---
+++ js/loops.js
@@ -0,0 +1,10 @@
+new Vue({
+    el:'#app',
+    data:{
+        people: [
+            {name: 'Allan Jeremy', age:24},
+            {name: 'Jessica Omija', age:23, occupation: 'student'},
+            {name: 'Mercy Dillon', age:23, occupation:'accountant'}
+        ]
+    }
+});
